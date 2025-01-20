80d2013bc065f045d2417c40e44838b9d016cea5
Rebecca Holt <beckyanneholt@gmail.com>
2025-01-19 18:44:15 -0700

    Complete Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,57 @@
 using System;
+using System.Transactions;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        //--new variable parking--
+        bool continueLoop = true;
+        float newNum = 0;
+        float total = 0;
+        float currentLargest = 0;
+        float average = 0;
+
+        List<float> numbers = new List<float>();
+        //------------------------
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        while (continueLoop)    // main loop
+        {
+            Console.Write("  > ");
+            string userInput = Console.ReadLine();      // get input
+            newNum = float.Parse(userInput);            // convert to float
+            
+            if (newNum != 0)
+            {
+                numbers.Add(newNum);
+            }
+            else    // exit mian loop
+            {
+                continueLoop = false;
+            }
+        }
+        Console.WriteLine();    //spacing
+
+        foreach (float num in numbers)  // compute total (sum) and find largest number
+        {
+            total += num;               //add num to sum (compute total)
+            
+            if (num > currentLargest)  // keep track of largest number
+            {
+                currentLargest = num;
+            }
+        }
+
+        average = total / numbers.Count;
+
+        Console.WriteLine(
+            $"The sum is: {total}\n" +
+            $"The average is: {average}\n" +
+            $"The largest number is {currentLargest}\n"
+        );
     }
 }
