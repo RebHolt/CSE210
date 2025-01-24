bd17e4046479207bf13cd70064c65a35d037ef09
Rebecca Holt <beckyanneholt@gmail.com>
2025-01-24 14:51:37 -0700

    Completed Learning Activity

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,17 @@
+using System.Security.AccessControl;
+
+public class Job
+{
+    public string _company = "";
+    public string _jobTitle = "";
+    public string _startYear = "";
+    public string _endYear = "";
+
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
+
+//company, jobtitle, startyear, end year
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        Resume resume1 = new Resume();
+
+        job1._company = "Co-op1";
+        job1._jobTitle = "service clerk";
+        job1._startYear = "2014";
+        job1._endYear = "2018";   
+
+        job2._company = "poop juice";
+        job2._jobTitle = "mixy person";
+        job2._startYear = "2024";
+        job2._endYear = "2025";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }
+        
